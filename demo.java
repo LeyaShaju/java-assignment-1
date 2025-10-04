6f4c7261a4a7964e84cbbb9f702b576deab2c7ea
Leya Shaju <leyshaju@gmail.com>
2025-10-04 11:01:53 +0100

    creating a 4x4 grid layout with 16 cards with image

--- demo.java
+++ demo.java
@@ -1,13 +1,15 @@
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class demo  implements ActionListener{
+public class demo implements ActionListener{
 
     JFrame frame=new JFrame();
     JPanel panel= new JPanel();
-    JButton button= new JButton();
+    JButton[] buttons =new JButton[16];
+    int numButtons = 16;
 
     demo(){
         buttonProperties();
@@ -18,16 +20,24 @@ public class demo  implements ActionListener{
         frame.setTitle("my window");
         frame.setSize(600,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.setLayout(new GridLayout(4,4,8,8));
+        
         frame.add(panel);
         frame.setVisible(true);
   
     }
     
     public void buttonProperties(){
-        button.setIcon(new ImageIcon("C:\\Users\\Leysh\\Second year\\Java\\java-assignment-1\\card_face_down.jpg"));
-        button.setPreferredSize(new java.awt.Dimension(200,300));
-        button.addActionListener(this);
-        panel.add(button);
+        for (int i = 0; i < numButtons; i++) {
+            buttons[i] = new JButton("Button" +(i +1));
+            buttons[i].setIcon(new ImageIcon("C:\\Users\\Leysh\\Second year\\Java\\java-assignment-1\\card_face_down.jpg"));
+            buttons[i].addActionListener(this);
+            panel.add(buttons[i]);
+        
+        panel.revalidate();
+        panel.repaint();
+        
+        }
 
 
     }
